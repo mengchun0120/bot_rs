35f095cdf33b7eaf677d9570b452435a4fdbce60
Chun Meng <mengchun0120@gmail.com>
2025-12-17 10:15:05 -0500

    Initialized with log & my_eror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod my_error;
+mod utils;
+
+use crate::utils::*;
+use bevy::{log::LogPlugin, prelude::*};
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let _guard = setup_log(&args.log_path);
+
+    info!("log has been setup");
+
+    App::new()
+        .add_plugins(DefaultPlugins.build().disable::<LogPlugin>())
+        .run();
+
 }
