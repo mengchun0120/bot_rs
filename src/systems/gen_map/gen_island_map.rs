39caca5dc3d299dc0bfa0b3b744ab00680356d8e
Chun Meng <mengchun0120@gmail.com>
2026-06-02 16:02:40 -0400

    Added tiles to island

--- src/systems/gen_map/gen_island_map.rs
+++ src/systems/gen_map/gen_island_map.rs
@@ -1,5 +1,8 @@
-use crate::config::{GameConfig, GenMapConfig};
-use crate::systems::gen_map::generated_map::{BotConfigPair, GeneratedMap, TileConfigPair};
+use crate::config::{GameConfig, GenMapConfig, IslandGenMapAlgorithm};
+use crate::systems::gen_map::generated_map::{
+    BotConfigPair, GeneratedMap, GeneratedMapItem, TileConfigPair,
+};
+use rand::seq::SliceRandom;
 use rand::{Rng, rng, rngs::ThreadRng};
 
 pub fn gen_island_map(
@@ -10,12 +13,98 @@ pub fn gen_island_map(
     tile_configs: &Vec<TileConfigPair>,
 ) -> Option<GeneratedMap> {
     let mut r = rng();
+
+    let mut map = GeneratedMap::new(
+        gen_map_config.row_count,
+        gen_map_config.col_count,
+        game_config.cell_size,
+    );
     let mut y = 0.0;
-    let end_x = gen_map_config.col_count as f32 * game_config.cell_size;
-    let end_y = gen_map_config.row_count as f32 * game_config.cell_size;
-    let mut map = GeneratedMap::new(gen_map_config.row_count, gen_map_config.col_count);
+
+    while true {
+        let mut x = 0.0;
+
+
+    }
 
     Some(map)
+}
+
+fn gen_island(
+    map: &mut GeneratedMap,
+    config: &IslandGenMapAlgorithm,
+    x: f32,
+    y: f32,
+    tile_configs: &Vec<TileConfigPair>,
+) -> Option<(f32, f32)> {
+    let Some((dist_x, dist_y, tile_index, tile_count_x, tile_count_y)) =
+        find_tile_for_island(map.width(), map.height(), config, x, y, tile_configs)
+    else {
+        return None;
+    };
+
+    if !add_tiles_to_island(
+        map,
+        x + dist_x,
+        y + dist_y,
+        tile_count_x,
+        tile_count_y,
+        &tile_configs[tile_index],
+    ) {
+        return None;
+    }
+
+    let span = tile_configs[tile_index].1.collide_span * 2.0;
+    let new_x = x + dist_x + tile_count_x as f32 * span;
+    let new_y = y + dist_y + tile_count_y as f32 * span;
+
+    Some((new_x, new_y))
+}
+
+fn find_tile_for_island(
+    width: f32,
+    height: f32,
+    config: &IslandGenMapAlgorithm,
+    x: f32,
+    y: f32,
+    tile_configs: &Vec<TileConfigPair>,
+) -> Option<(f32, f32, usize, usize, usize)> {
+    let mut tile_indices: Vec<usize> = (0..tile_configs.len()).collect();
+    let mut r = rng();
+
+    tile_indices.shuffle(&mut r);
+
+    for i in tile_indices {
+        let tile_span = 2.0 * tile_configs[i].1.collide_span;
+        let Some((dist_x, tile_count_x)) = get_island_gap_span(
+            &mut r,
+            x,
+            width,
+            tile_span,
+            config.min_island_dist,
+            config.max_island_dist,
+            config.min_island_width,
+            config.max_island_width,
+        ) else {
+            continue;
+        };
+        let Some((dist_y, tile_count_y)) = get_island_gap_span(
+            &mut r,
+            y,
+            height,
+            tile_span,
+            config.min_island_dist,
+            config.max_island_dist,
+            config.min_island_height,
+            config.max_island_height,
+        ) else {
+            continue;
+        };
+
+        return Some((dist_x, dist_y, i, tile_count_x, tile_count_y));
+    }
+
+    None
 }
 
 fn get_island_gap_span(
@@ -49,4 +138,29 @@ fn get_island_gap_span(
     let island_tile_count = r.random_range(min_island_tile_count..=max_island_tile_count);
 
     Some((island_dist, island_tile_count))
+}
+
+fn add_tiles_to_island(
+    map: &mut GeneratedMap,
+    x: f32,
+    y: f32,
+    tile_count_x: usize,
+    tile_count_y: usize,
+    tile_config: &TileConfigPair,
+) -> bool {
+    let span = tile_config.1.collide_span * 2.0;
+    let mut y = y + tile_config.1.collide_span;
+
+    for _ in 0..tile_count_y {
+        let mut x1 = x + tile_config.1.collide_span;
+        for _ in 0..tile_count_x {
+            if !map.add(x1, y, GeneratedMapItem::Tile(tile_config.clone())) {
+                return false;
+            }
+            x1 += span;
+        }
+        y += span;
+    }
+
+    true
 }
--- src/systems/gen_map/generated_map.rs
+++ src/systems/gen_map/generated_map.rs
@@ -1,5 +1,5 @@
 use crate::config::{BotConfig, TileConfig};
-use crate::game_utils::MapRegion;
+use bevy::prelude::*;
 
 pub type BotConfigPair = (String, BotConfig);
 pub type TileConfigPair = (String, TileConfig);
@@ -10,14 +10,54 @@ pub enum GeneratedMapItem {
     Tile(TileConfigPair),
 }
 pub struct GeneratedMap {
+    cell_size: f32,
     map: Vec<Vec<Vec<GeneratedMapItem>>>,
 }
 
 impl GeneratedMap {
-    pub fn new(row_count: usize, col_count: usize) -> Self {
+    pub fn new(row_count: usize, col_count: usize, cell_size: f32) -> Self {
         Self {
+            cell_size,
             map: vec![vec![vec![]; col_count]; row_count],
         }
+    }
+
+    #[inline]
+    pub fn row_count(&self) -> usize {
+        self.map.len()
+    }
+
+    #[inline]
+    pub fn col_count(&self) -> usize {
+        self.map[0].len()
+    }
+
+    #[inline]
+    pub fn width(&self) -> f32 {
+        self.col_count() as f32 * self.cell_size
+    }
+
+    #[inline]
+    pub fn height(&self) -> f32 {
+        self.row_count() as f32 * self.cell_size
+    }
+
+    #[inline]
+    pub fn get_index(&self, c: f32) -> usize {
+        (c / self.cell_size).floor() as usize
+    }
+
+    pub fn add(&mut self, x: f32, y: f32, item: GeneratedMapItem) -> bool {
+        let row = self.get_index(y);
+        let col = self.get_index(x);
+        if !(0..self.col_count()).contains(&row) || !(0..self.row_count()).contains(&col) {
+            error!("Cannot add map item {:?} at ({}, {})", item, x, y);
+            return false;
+        }
+
+        self.map[row][col].push(item);
+
+        true
     }
 
     #[inline]
